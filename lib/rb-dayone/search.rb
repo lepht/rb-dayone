9f8aee385a179c371522d19d7316f40c717144a3
Jan-Yves Ruzicka <janyves.ruzicka@gmail.com>
2012-08-16 20:53:28 +1200

    Added cache to search

--- lib/rb-dayone/search.rb
+++ lib/rb-dayone/search.rb
@@ -15,8 +15,7 @@ class DayOne::Search
   def results
     if !@results
       @results = []
-      entries = File.join(DayOne::journal_location, 'entries', '*.doentry')
-      working_results = Dir[entries].map{ |e| DayOne::EntryImporter::from_file(e).to_entry }
+      working_results = cache
 
       working_results = working_results.select{ |e| e.entry_text.include? entry_text } unless entry_text == ''
       working_results = working_results.select{ |e| e.starred == starred } unless starred.nil?
@@ -28,5 +27,18 @@ class DayOne::Search
   
   def [] index
     results[index]
+  end
+  
+  private
+  
+  # Caches all journal entries for us. On first running, will load everything
+  # from file. After this, will just refer back to the cache.
+  # @return [Array] an array of DayOne::Entry objects
+  def cache
+    if !@cache
+      entries = File.join(DayOne::journal_location, 'entries', '*.doentry')
+      @cache = Dir[entries].map{ |e| DayOne::EntryImporter::from_file(e).to_entry }
+    end
+    @cache
   end
 end
